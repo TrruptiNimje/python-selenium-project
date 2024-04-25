45839370aff868cc2557bb2ff8c2a9d9f32ba552
TrruptiNimje <nimje.trupti5@gmail.com>
2024-04-25 12:50:52 +0200

    Adding parametrized fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,38 @@
+import time
+
 import pytest
 import selenium
 
 
-@pytest.fixture()
-def driver():
-    print("Creating chrome driver")
-    # my_driver = selenium.webdriver.Safari()
-    my_driver = selenium.webdriver.Chrome()
+@pytest.fixture(params=["chrome", "safari"])
+def driver(request):
+
+    # browser = request.config.getoption("--browser") >> enable this for command-line execution
+
+    browser = request.param
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = selenium.webdriver.Chrome()
+    elif browser == "safari":
+        my_driver = selenium.webdriver.Safari()
+        time.sleep(5)
+    else:
+        raise TypeError(f"Expected 'chrome' or 'safari', but git {browser}")
     yield my_driver
-    print("Closing chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or safari)"
+    )
+
+
+"""
+    "--browser", action="store",  ****"
+    This code line is for commandline test execution.
+    example of how to run from commandline: 
+    'pytest --browser safari test_name'
+
+"""
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -8,25 +8,29 @@ class TestPositiveScenarios:
     @pytest.mark.login
     @pytest.mark.positivetest
     def test_positive_login(self, driver):
-
         # Go to webpage
         driver.get("https://practicetestautomation.com/practice-test-login/")
+        time.sleep(2)
 
         # Type username student into Username field
         username_locator = driver.find_element(By.ID, "username")
         username_locator.send_keys("student")
+        time.sleep(2)
 
         # Type password Password123 into Password field
         password_locator = driver.find_element(By.NAME, "password")
         password_locator.send_keys("Password123")
+        time.sleep(2)
 
         # Push Submit button
         submit_button_locator = driver.find_element(By.XPATH, "//button[@id='submit']")
         submit_button_locator.click()
+        time.sleep(2)
 
         # Verify new page URL contains practicetestautomation.com/logged-in-successfully/
         actual_url = driver.current_url
         assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+        time.sleep(2)
 
         # Verify new page contains expected text ('Congratulations' or 'successfully logged in')
         text_locator = driver.find_element(By.TAG_NAME, "h1")
